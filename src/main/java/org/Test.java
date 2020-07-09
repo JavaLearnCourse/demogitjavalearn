914a5f14aecd9863519489a554a475567d8ba3c4
Rafael <rfl777@mail.ru>
2020-07-09 21:37:13 +0300

    add static and run medhod

--- src/main/java/org/Test.java
+++ src/main/java/org/Test.java
@@ -4,10 +4,10 @@ public class Test {
 
 
     public static void main(String[] args) {
-
+        sum(5);
     }
 
-    public  void sum(int n){
+    public static void sum(int n){
         int sum = 0;
         for (int i = 1; i <= n; i++){
             sum += i;
