7b87892597b3ffd9e357de4375af61e9ac1d8d01
Dmitry Savkin <dmitry.savkin.info@gmail.com>
2020-07-09 20:10:29 +0200

    Init commit

---
+++ src/main/java/org/Test.java
@@ -0,0 +1,17 @@
+package org;
+
+public class Test {
+
+
+    public static void main(String[] args) {
+
+    }
+
+    public  void sum(int n){
+        int sum = 0;
+        for (int i = 1; i <= n; i++){
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
