e375b4df3b4f1430cb81559f31ebd1d589644eea
Владислав Зейналов <zeynalovmail@mail.ru>
2020-09-06 17:08:03 +0300

    init 2 Classes with methods

---
+++ src/main/java/org/SecondClass.java
@@ -0,0 +1,9 @@
+package org;
+
+public class SecondClass {
+    public static void main(String[] args) {
+        FirstClass firstClass = new FirstClass();
+        boolean newVar = true;
+        firstClass.returnMe(newVar);
+    }
+}
